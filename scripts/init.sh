85f6ebcc1d531a67092aaec52d3db70b77fa96c8
ciandcd2017 <33650239+ciandcd2017@users.noreply.github.com>
2018-06-11 10:02:19 +0530

    echo command for testing

--- scripts/init.sh
+++ scripts/init.sh
@@ -4,8 +4,10 @@ export PATH=/Selenium/bin/:$PATH
 Xvfb :10 -ac &
 export DISPLAY=:10
 
+echo $SCRIPT_PATH_NAME
 sed -i '/implicitly/d' $SCRIPT_PATH_NAME
 sed -i 's/${TARGET_HOST}/'$TARGET_HOST'/g' $SCRIPT_PATH_NAME
 sed -i 's/${TARGET_PORT}/'$TARGET_PORT'/g' $SCRIPT_PATH_NAME
 
+echo $SCRIPT_PATH_NAME
 python $SCRIPT_PATH_NAME
